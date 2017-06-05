ab072b184d1e173c68bab1c3e52bb4601de9c088
Bart Gysens <bart.gysens@kta1-hasselt.be>
2017-06-05 14:07:38 +0200

    Module stages uitgebreid met controle van stagebezoeken

---
+++ sites/all/modules/argus_stages/templates/argus_stages_bezoeken--controle.tpl.php
@@ -0,0 +1,78 @@
+<?php
+/* 
+ * Copyright (C) 2016 bartgysens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+
+<div style="text-align: right;">
+	<span class="argus_stages-view-legend" style="background-color: red;"></span> verslag niet ok
+	<span class="argus_stages-view-legend" style="background-color: orange;"></span> registratie na bezoek
+	<span class="argus_stages-view-legend" style="background-color: green;"></span> nog uit te voeren
+</div>
+<table class="argus_uurrooster_schedule views-table sticky-enabled cols-4 tableheader-processed sticky-table" style="page-break-after: always;">
+    <thead>
+    	<tr>
+		    <th class="views-field views-align-center" scope="col" rowspan="2">Nr.</td>
+		    <th class="views-field views-align-left" scope="col" <?php if ($sorter == 'begeleider') print "style='font-weight: bold'"; ?>><a href="?s=begeleider"><?php print t('STAGEBEGELEIDER'); ?></a></td>
+		    <th class="views-field views-align-left" scope="col" <?php if ($sorter == 'leerling') print "style='font-weight: bold'"; ?>><a href="?s=leerling"><?php print t('LEERLING'); ?></a></td>
+		    <th class="views-field views-align-center" scope="col" <?php if ($sorter == 'klas') print "style='font-weight: bold'"; ?>><a href="?s=klas"><?php print t('KLAS'); ?></a></td>
+		    <th class="views-field views-align-center" scope="col"><?php print t('STAGEBEZOEK'); ?></td>
+		    <th class="views-field views-align-center" scope="col"><?php print t('REGISTRATIE'); ?></td>
+		    <th class="views-field views-align-right" scope="col"><?php print t('ACTIES'); ?></td>
+		</tr>
+    </thead>
+    <tbody>
+	<?php
+	
+	$cntr = 1;
+	foreach ($stagebezoeken as $sbid => $r){
+		print '<tr class="'.($cntr%2 == 0 ? "odd" : "even").' views-row-first">';
+		
+		print '<td class="views-field views-align-center">'.($cntr++).'</td>';
+		
+		print '<td class="views-field views-align-left"><a href="'.base_path().drupal_lookup_path('alias', 'user/'.$r['begeleider']).'" target="_blank">'.argus_get_user_realname($r['begeleider']).'</a></td>';
+
+		print '<td class="views-field views-align-left"><a href="'.base_path().drupal_lookup_path('alias', 'user/'.$r['leerling']).'" target="_blank">'.argus_get_user_realname($r['leerling']).'</a></td>';
+
+		print '<td class="views-field views-align-center">'.$r['klas'].'</td>';
+
+		$visit = new DateTime ($r['tijdstip']);
+		$register = new DateTime ();
+		$register->setTimestamp($r['register']);
+		
+		if (date_diff($register, $visit)->invert){
+			$color = 'orange';
+		} else {
+			if (date_diff($visit, new DateTime())->invert){
+				$color = 'green';
+			} else {
+				$color = 'red';
+			}
+		}
+		
+		print '<td class="views-field views-align-center" style="color: '.$color.'">'.format_date($visit->getTimestamp(), 'short').'</td>';
+		
+		print '<td class="views-field views-align-center">'.format_date($register->getTimestamp(), 'short').'</td>';
+		
+		print '<td class="views-field views-align-right"><a href="'.base_path().'node/'.$sbid.'/edit">'.t('bewerken').'</a></td>';
+
+		print '</tr>';
+	}
+	
+	?>
+	</tbody>
+</table>
+<div style="font-size: smaller; text-align: center;"><u>Opgelet</u>: indien je je bezoek niet registreert op voorhand, dan ben je verzekeringstechnisch niet in orde! (code oranje)</div>
---
+++ sites/all/modules/argus_stages/templates/argus_stages_bezoeken_controle--filter_block.tpl.php
@@ -0,0 +1,153 @@
+<?php
+
+/* 
+ * Copyright (C) 2015 bartgysens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+global $base_url;
+
+/* Select user based upon the role selected */
+$bundles = array('uurrooster_vervanging', 'uurrooster_permanentie', 'uurrooster_les', 'uurrooster_toezicht');
+$query = 'SELECT DISTINCT(u.uid) AS id '
+		. 'FROM {users} AS u '
+		. 'INNER JOIN {field_data_field_user_sms_naam} AS un ON u.uid = un.entity_id '
+		. 'INNER JOIN {field_data_field_user_sms_voornaam} AS uv ON u.uid = uv.entity_id '
+		. 'LEFT JOIN {field_data_field_uurrooster_les_leerkracht} AS l ON l.field_uurrooster_les_leerkracht_target_id = u.uid '
+		. 'LEFT JOIN {field_data_field_uurrooster_perm_vervanger} AS p ON p.field_uurrooster_perm_vervanger_target_id = u.uid '
+		. 'LEFT JOIN {field_data_field_uurrooster_toez_toezichter} AS t ON t.field_uurrooster_toez_toezichter_target_id = u.uid ';
+if (module_exists('argus_uurrooster_vervanging')){
+		$query .= 'LEFT JOIN {field_data_field_gebruiker} AS g ON g.field_gebruiker_target_id = u.uid ';
+		$bundle[] = 'uurrooster_vervanging';
+}
+$query .= 'WHERE status = 1 '
+		. 'AND (';
+if (module_exists('argus_uurrooster_vervanging')){
+		$query .= 'g.bundle IN (:bundle) OR ';
+}
+$query .= 'l.bundle IN (:bundle) OR p.bundle IN (:bundle) OR t.bundle IN (:bundle)) '
+		. 'ORDER BY un.field_user_sms_naam_value ASC, uv.field_user_sms_voornaam_value ASC';
+$users_lkr = db_query($query, array(':bundle' => $bundles))->fetchAll();
+
+$query = 'SELECT DISTINCT(u.uid) AS id '
+		. 'FROM {users} AS u '
+		. 'INNER JOIN {users_roles} AS ur ON u.uid = ur.uid '
+		. 'INNER JOIN {field_data_field_user_sms_naam} AS un ON u.uid = un.entity_id '
+		. 'INNER JOIN {field_data_field_user_sms_voornaam} AS uv ON u.uid = uv.entity_id '
+		. 'WHERE ur.rid IN (:rids) AND status = 1 '
+		. 'ORDER BY un.field_user_sms_naam_value ASC, uv.field_user_sms_voornaam_value ASC';
+$users_lln = db_query($query, array(':rids' => variable_get('argus_engine_roles_pupil')))->fetchAll();
+?>
+
+<div class="menu-block-wrapper">
+    <form>
+        <strong>Klassen</strong><br />
+            <select id="argus_uurrooster_change_classes">
+            <?php 
+            echo '<option value="">-</option>';
+            $query = 'SELECT nid, title ' .
+					'FROM {node} AS k ' .
+					'INNER JOIN {field_data_field_klas_leerlingen} AS lln ON k.nid = lln.entity_id ' .
+					'WHERE k.status = 1 ' .
+					'ORDER BY k.title ASC';
+			$classes = db_query ( $query )->fetchAllKeyed();
+			if (count( $classes ) > 0) {
+                foreach ($classes as $key => $c) {
+                    echo '<option value="'.$key.'"';
+                    if (isset($cid)) {
+                        if ($key == $cid) {
+                            echo ' selected';
+                        }
+                    }
+                    echo '>'.$c.'</option>';
+                }
+            } ?>
+            </select><br />
+
+        <strong>Leerkrachten / opvoeders</strong><br />
+            <select id="argus_uurrooster_change_user_lkr">
+            <?php 
+            echo '<option value="">-</option>';
+            foreach ($users_lkr as $u) {
+            	echo '<option value="'.$u->id.'"';
+                if (isset($uid)) {
+                    if ($u->id == $uid) {
+                        echo ' selected';
+                    }
+                }
+            	echo '>'.argus_get_user_realname($u->id).'</option>';
+            } ?>
+            </select><br />
+
+        <strong>Leerlingen</strong><br />
+            <select id="argus_uurrooster_change_user_lln">
+            <?php 
+            echo '<option value="">-</option>';
+            foreach ($users_lln as $u) {
+                echo '<option value="'.$u->id.'"';
+                if (isset($uid)) {
+                    if ($u->id == $uid) {
+                        echo ' selected';
+                    }
+                }
+                echo '>'.argus_get_user_realname($u->id).'</option>';
+            } ?>
+            </select><br />
+
+        <strong>Lokalen</strong><br />
+            <select id="argus_uurrooster_change_rooms">
+            <?php 
+            echo '<option value="">-</option>';
+            $query = new EntityFieldQuery();
+            $query->entityCondition('entity_type', 'node')
+                ->entityCondition('bundle', 'lokaal')
+                ->propertyCondition('status', NODE_PUBLISHED)
+                ->propertyOrderBy('title', 'ASC');
+            $rooms = $query->execute();
+            if (isset($rooms)) {
+            	if (array_key_exists('node', $rooms)){
+	                foreach ($rooms['node'] as $key => $r) {
+	                    $r = (array) node_load($key);
+	                    echo '<option value="'.$key.'"';
+	                    if (isset($rid)) {
+	                        if ($key == $rid) {
+	                            echo ' selected';
+	                        }
+	                    }
+	                    echo '>'.$r['title'].'</option>';
+	                }
+            	}
+            } ?>
+            </select>
+		<hr />
+		<h4>Andere roosters:</h4>
+		<ul>
+			<li><a href="<?php print $base_url; ?>/uurrooster/permanentierooster">Permanentierooster</a></li>
+			<li><a href="<?php print $base_url; ?>/uurrooster/toezichtenrooster">Toezichtenrooster</a></li>
+			<?php if (module_exists('argus_uurrooster_vervanging')) { ?>
+				<li><a href="<?php print $base_url; ?>/uurrooster/vervangingen">Vervangingen</a></li>
+			<?php } ?>
+			<li><a href="<?php print $base_url; ?>/uurrooster/beschikbare-leerkrachten">Beschikbare leerkrachten</a></li>
+			<li><a href="<?php print $base_url; ?>/uurrooster/beschikbare-lokalen">Vrije lokalen</a></li>
+			<li><a href="<?php print $base_url; ?>/uurrooster/lokaalbezetting">Lokaalbezetting</a></li>
+		</ul>
+		
+		<?php 
+		if( taxonomy_get_term_by_name('uurrooster')){
+			echo '<div style="font-size: smaller; text-align: right;"><a href="'.$base_url.'/tags/uurrooster">meer over uurroosters</a></div>';
+		}
+		?>
+    </form>
+</div>
